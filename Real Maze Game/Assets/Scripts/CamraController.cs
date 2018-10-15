2f57d8611b7b6430d15d4931acbbd7cb000d0597
aquacat <andernoha@gmail.com>
2018-10-14 23:23:59 -0400

    Ad game files

---
+++ Real Maze Game/Assets/Scripts/CamraController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class CamraController : MonoBehaviour
+{
+
+    public GameObject You;
+    public Vector3 offset;
+
+    public void Start()
+    {
+        offset = transform.position - You.transform.position;
+    }
+
+    public void LateUpdate()
+    {
+        transform.position = You.transform.position + offset;
+    }
+}
---
+++ Real Maze Game/Assets/Scripts/Timer.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Timer : MonoBehaviour
+{
+    [SerializeField]
+    public Text timer;
+   
+
+
+
+    float minutes, seconds, miliseconds;
+
+    private float intTime;
+
+    public Text label;
+    private readonly Rigidbody rb;
+
+    // Update is called once per frame
+    void Update()
+    {
+        intTime += Time.deltaTime;
+
+        minutes = intTime / 60;
+        seconds = intTime % 60;
+        miliseconds = Time.deltaTime * 1000;
+        miliseconds = miliseconds % 1000;
+
+        if (label != null)
+        {
+            label.text = /*"" +*/ (int)minutes + ":" + (int)seconds;
+        }
+    }
+   
+}
